6278635eb6084ed8db56a2a39f0fb1067ba8f6d2
Gero Kassing <gero.codes@gmail.com>
2021-09-10 15:50:26 +0200

    fix(helpers.js): logic error mdd

--- src/Service/helpers.js
+++ src/Service/helpers.js
@@ -7,18 +7,24 @@ const calculateMDD = (data) => {
   data.reverse();
 
   for (let i = 0; i < data.length; i++) {
+    // edge case - last el of arr.
     if (data[i + 1] !== undefined) {
+      // check if cur adj close is greater
       if (data[i][11] > data[i + 1][11] && data[i][11] > curPeak.value) {
+        // set new peak, reset trough (prev trough unvalid)
         curPeak = { timestamp: data[i][0], value: data[i][11] };
         curTrough = { timestamp: '0', value: undefined };
       }
 
       if (
-        curPeak.value !== 0 &&
-        data[i][11] < data[i + 1][11] &&
-        curPeak.timestamp < data[i][0]
+        curPeak.value !== 0 && // there is a peak, so trough is possible
+        data[i][11] < data[i + 1][11] && // next val is larger, so it is a tough
+        curPeak.timestamp < data[i][0] // trough comes after peak
       ) {
-        if (!(curTrough.value !== undefined && curTrough.value < data[i][0])) {
+        if (
+          curTrough.value === undefined || // there is no prev trough
+          (curTrough.value !== undefined && data[i][11] < curTrough.value) // prev trough is valid, but less deep
+        ) {
           curTrough = { timestamp: data[i][0], value: data[i][11] };
 
           curDrawDown =
